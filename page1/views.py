511e4f609e3e1d6e9cf503df7e49dcf03deb1500
leopard1982 <99190737+leopard1982@users.noreply.github.com>
2022-11-10 09:48:22 +0700

    initial 10.11.2022

--- page1/views.py
+++ page1/views.py
@@ -2,37 +2,18 @@ from django.shortcuts import render, HttpResponse
 from .forms import InputDaftarNama
 from .models import DaftarNama
 
-from django.views import View
-
-from django.views.generic.list import ListView
-
+from django.views import generic
 
 # Create your views here.
-class Parameterku(View):
-    parameters = None
-    def post(self,request,parameter):
-        return HttpResponse("Dari POST, parameternya itu ya: " + parameter)
-
-    def get(self,request,parameter):
-        return HttpResponse("Dari GET, parameternya adalah: " + parameter)
-
-class IndexViewClass(View):
-    hasil=False
-    def post(self,request):
+def indeks(request):
+    if request.method == 'POST':
         forms = InputDaftarNama(request.POST)
         if forms.is_valid():
             forms.save()
-        forms = InputDaftarNama(initial={'nama':request.POST['nama'],'alamat':request.POST['alamat']})
-        data = DaftarNama.objects.all()
-        return render(request,'indeks.html',{'forms':forms,'data':data,'hasil':self.hasil})
-    
-    def get(self,request):
-        forms = InputDaftarNama(initial={
-            'nama':'xxxxxx',
-            'alamat':'yyyyyy'
-            })
-        data = DaftarNama.objects.all()
-        return render(request,'indeks.html',{'forms':forms,'data':data,'hasil':self.hasil}) 
+
+    forms = InputDaftarNama()
+    data = DaftarNama.objects.all()[2:3]
+    return render(request,'indeks.html',{'forms':forms,'data':data})
 
 def indeks2(request):
     if request.method == 'POST':
